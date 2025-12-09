7e06b894f4219efbce00af33b2e0d5980f8d81d8
auzexo <auzexotech@gmail.com>
2025-12-09 08:12:19 +0530

    Delete web/pages/api/tournaments/create.js

--- web/pages/api/tournaments/create.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req,res){
-  if(req.method !== 'POST') return res.status(405).end();
-  // This endpoint should be implemented server-side to create tournaments
-  res.json({ ok:true, message: 'Create tournament endpoint (stub)' });
-}
