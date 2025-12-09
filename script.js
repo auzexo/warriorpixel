2c3579e98bb4d3f1338c664f801150b8a1dcc170
auzexo <auzexotech@gmail.com>
2025-12-10 05:12:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Homepage loaded successfully");
+const tabs = document.querySelectorAll('.sidebar-item');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+    });
+});
